848ba95cc851aca4710974affb70f44d686ae1b8
Leonardo Schmitt <antoniollesnik@gmil.com>
2025-03-05 17:12:24 -0300

    projeto exercicio9 Desenvolvido pelo Leonardo schmitt data:28/02/2025 Primeiro commit

---
+++ Documents/NetBeansProjects/ManipulacaoString2/src/manipulacaostring2/ManipulacaoString2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manipulacaostring2;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aluno.saolucas
+ */
+public class ManipulacaoString2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite uma palavra:");
+       
+        int tamanho = palavra.lenght();
+    }
+    
+   
+}
---
+++ Documents/NetBeansProjects/ManipulacaoString2/src/manipulacaostring2/palavra.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manipulacaostring2;
+
+/**
+ *
+ * @author aluno.saolucas
+ */
+class palavra {
+
+    static int lenght() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
